7838c05044bf2b662f6ee31b2514b70ac151b78f
Ninh Dong <ninhdongnguyen@gmail.com>
2023-12-10 19:38:37 +0700

    Add some new page like customer, profile, etc

---
+++ FinalProject_.Net/Pages/customer/OrderDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject_.Net.Pages.customer
+{
+    public class OrderDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
